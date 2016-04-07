86970ab75edf86130abdda65f08f2bc699b6a13e
cheng93 <derek.c@hotmail.co.uk>
2016-04-07 17:45:14 +0100

    Initial tests

--- Argon2015/Argon2015.UnitTest/CodilityTest.cs
+++ Argon2015/Argon2015.UnitTest/CodilityTest.cs
@@ -8,8 +8,25 @@ namespace Argon2015.UnitTest
         private readonly Solution _solution = new Solution();
 
         [TestMethod]
-        public void TestMethod1()
+        public void Example1()
         {
+            var a = new[] {1, 1, 0, 1, 0, 0, 1, 1};
+
+            var actual = _solution.Solve(a);
+            var expected = 7;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var a = new[] { 1, 0 };
+
+            var actual = _solution.Solve(a);
+            var expected = 0;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
