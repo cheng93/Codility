47304560246484e2dba1fa0cc8ed54660bab86e8
cheng93 <derek.c@hotmail.co.uk>
2016-04-07 19:03:23 +0100

    Finished solution

--- Argon2015/Argon2015/Solution.cs
+++ Argon2015/Argon2015/Solution.cs
@@ -6,23 +6,42 @@ namespace Argon2015
     {
         private readonly IForecastsChange _forecastsChange;
 
-        public Solution(IForecastsChange forecastsChange)
+        private readonly IForecastCounter _swimmingForecastCounter;
+
+        private readonly IForecastCounter _trekkingForecastCounter;
+
+        public Solution(IForecastsChange forecastsChange, IForecastCounter swimmingForecastCounter, IForecastCounter trekkingForecastCounter)
         {
             _forecastsChange = forecastsChange;
+            _swimmingForecastCounter = swimmingForecastCounter;
+            _trekkingForecastCounter = trekkingForecastCounter;
         }
 
         public int Solve(int[] a)
         {
-            var daysWithChange = _forecastsChange.Get(a);
+            var daysWithChange = _forecastsChange.Get(a).ToList();
 
-            if (!daysWithChange.Any()) return 0;
-            
-            throw new System.NotImplementedException();
+            if (daysWithChange.Count == 0) return 0;
+
+            var holidayDays = 0;
+
+            foreach (var day in daysWithChange)
+            {
+                var swimmingDays = _swimmingForecastCounter.Count(a.Take(day + 1).Reverse());
+                var trekkingDays = _trekkingForecastCounter.Count(a.Skip(day + 1));
+
+                if (swimmingDays + trekkingDays > holidayDays)
+                {
+                    holidayDays = swimmingDays + trekkingDays;
+                }
+            }
+
+            return holidayDays;
         }
 
         public static Solution Create()
         {
-            return new Solution(new ForecastsChange());
+            return new Solution(new ForecastsChange(), new SwimmingForecastCounter(), new TrekkingForecastCounter());
         }
     }
 }
