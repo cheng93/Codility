922080a4febcf4ef338f5bcb8c73e369c08971f3
cheng93 <derek.c@hotmail.co.uk>
2016-04-07 19:30:13 +0100

    Added tests

--- Argon2015/Argon2015.UnitTest/SwimmingForecastCounterTest.cs
+++ Argon2015/Argon2015.UnitTest/SwimmingForecastCounterTest.cs
@@ -9,12 +9,67 @@ namespace Argon2015.UnitTest
         private readonly IForecastCounter _forecastCounter = new SwimmingForecastCounter();
 
         [TestMethod]
-        public void Empty()
+        public void Single()
         {
-            var forecasts = new List<int> {};
+            var forecasts = new List<int> { 0 };
+
+            var actual = _forecastCounter.Count(forecasts);
+            var expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Single_BadWeather()
+        {
+            var forecasts = new List<int> { 1 };
 
             var actual = _forecastCounter.Count(forecasts);
             var expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Equal()
+        {
+            var forecasts = new List<int> { 0, 1 };
+
+            var actual = _forecastCounter.Count(forecasts);
+            var expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExtraDays()
+        {
+            var forecasts = new List<int> { 0, 1, 0 };
+
+            var actual = _forecastCounter.Count(forecasts);
+            var expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExtraExtraDays()
+        {
+            var forecasts = new List<int> { 0, 1, 0, 0 };
+
+            var actual = _forecastCounter.Count(forecasts);
+            var expected = 4;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BadHeavyDays()
+        {
+            var forecasts = new List<int> { 0, 1, 1, 0 };
+
+            var actual = _forecastCounter.Count(forecasts);
+            var expected = 1;
 
             Assert.AreEqual(expected, actual);
         }
