86d220ed14032a183068de33c16c394d8537b836
cheng93 <derek.c@hotmail.co.uk>
2016-04-07 18:15:52 +0100

    IForecastsChange and its implementation

---
+++ Argon2015/Argon2015/ForecastsChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Argon2015.UnitTest")]
+namespace Argon2015
+{
+    internal class ForecastsChange : IForecastsChange
+    {
+        public IEnumerable<int> Get(int[] forecasts)
+        {
+            if (forecasts.Length < 2) return new List<int> {};
+
+            return forecasts.Where((f, i) => i != forecasts.Length - 1 && f == 0 && forecasts[i + 1] == 1);
+        }
+    }
+}
---
+++ Argon2015/Argon2015/IForecastsChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Argon2015
+{
+    public interface IForecastsChange
+    {
+        /// <summary>
+        /// Gets the indexes of the array when the next value has changed.
+        /// </summary>
+        /// <param name="forecasts"></param>
+        /// <returns></returns>
+        IEnumerable<int> Get(int[] forecasts);
+    }
+}
