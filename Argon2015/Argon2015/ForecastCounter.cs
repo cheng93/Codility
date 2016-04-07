2ff9ffa68bdef3a0115b5dd371bbf534b785bc1d
cheng93 <derek.c@hotmail.co.uk>
2016-04-07 19:42:06 +0100

    Implement Count

--- Argon2015/Argon2015/ForecastCounter.cs
+++ Argon2015/Argon2015/ForecastCounter.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Argon2015
 {
@@ -14,7 +14,42 @@ namespace Argon2015
 
         public int Count(IEnumerable<int> forecasts)
         {
-            throw new NotImplementedException();
+            var count = 1;
+            var runningCount = 0;
+            var goodForecastCount = 0;
+            var badForecastCount = 0;
+
+            foreach (var forecast in forecasts.Skip(1))
+            {
+                if (forecast == _goodForecast)
+                {
+                    goodForecastCount++;
+                    if (goodForecastCount <= badForecastCount)
+                    {
+                        runningCount += 2;
+                        goodForecastCount--;
+                        badForecastCount--;
+                    }
+                }
+                else
+                {
+                    badForecastCount++;
+                    if (badForecastCount <= goodForecastCount)
+                    {
+                        runningCount += 2;
+                        goodForecastCount--;
+                        badForecastCount--;
+                    }
+                }
+
+                if (badForecastCount == 0)
+                {
+                    count += runningCount;
+                    runningCount = 0;
+                }
+            }
+
+            return count + goodForecastCount;
         }
     }
 }
