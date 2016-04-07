ba8828ec2079803513b94b91ee3d6c0185fa53f5
cheng93 <derek.c@hotmail.co.uk>
2016-04-07 18:23:48 +0100

    Static factory, implement solve, IForecastsChange field

--- Argon2015/Argon2015/Solution.cs
+++ Argon2015/Argon2015/Solution.cs
@@ -1,10 +1,28 @@
-﻿namespace Argon2015
+﻿using System.Linq;
+
+namespace Argon2015
 {
     public class Solution
     {
+        private readonly IForecastsChange _forecastsChange;
+
+        public Solution(IForecastsChange forecastsChange)
+        {
+            _forecastsChange = forecastsChange;
+        }
+
         public int Solve(int[] a)
         {
+            var daysWithChange = _forecastsChange.Get(a);
+
+            if (!daysWithChange.Any()) return 0;
+            
             throw new System.NotImplementedException();
+        }
+
+        public static Solution Create()
+        {
+            return new Solution(new ForecastsChange());
         }
     }
 }
