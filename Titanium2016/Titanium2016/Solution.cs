492266b33e8077a4b5cb921c80cb9bd4a8e28608
cheng93 <derek.c@hotmail.co.uk>
2016-08-26 20:24:49 +0100

    Start of solution.

--- Titanium2016/Titanium2016/Solution.cs
+++ Titanium2016/Titanium2016/Solution.cs
@@ -2,5 +2,13 @@
 {
     public class Solution
     {
+        public int Solve(string s, int k)
+        {
+            if (s == ")" || s == "(")
+            {
+                return 0;
+            }
+            return 0;
+        }
     }
 }
