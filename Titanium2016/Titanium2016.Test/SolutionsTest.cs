15f8c3d0644936515eb86ad471b5b6936a154e03
cheng93 <derek.c@hotmail.co.uk>
2016-08-26 20:24:37 +0100

    My tests

---
+++ Titanium2016/Titanium2016.Test/SolutionsTest.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+namespace Titanium2016.Test
+{
+    [TestFixture]
+    public class SolutionsTest
+    {
+        private readonly Solution _solution = new Solution();
+
+        private string _s;
+
+        private int _k;
+
+        public void Single()
+        {
+            _s = ")";
+            _k = 1;
+
+            var expected = 0;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void DoubleValid()
+        {
+            _s = "()";
+            _k = 0;
+
+            var expected = 2;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void DoubleInvalid_OneTurn()
+        {
+            _s = "((";
+            _k = 1;
+
+            var expected = 2;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void DoubleInvalid_TwoTurns()
+        {
+            _s = "((";
+            _k = 2;
+
+            var expected = 2;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void DoubleInvalid_NoTurns()
+        {
+            _s = "((";
+            _k = 0;
+
+            var expected = 0;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
