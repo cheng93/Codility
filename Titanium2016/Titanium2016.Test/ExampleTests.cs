8aee89441bb7cfb3856de37bbda6b0199754f237
cheng93 <derek.c@hotmail.co.uk>
2016-08-26 20:24:29 +0100

    Tests provided by Codility

--- Titanium2016/Titanium2016.Test/ExampleTests.cs
+++ Titanium2016/Titanium2016.Test/ExampleTests.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace Titanium2016.Test
 {
-    [TestClass]
+    [TestFixture]
     public class ExampleTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly Solution _solution = new Solution();
+
+        private string _s;
+
+        private int _k;
+
+        [Test]
+        public void Test1()
         {
+            _s = ")()()(";
+            _k = 3;
+
+            var actual = _solution.Solve(_s, _k);
+            var expected = 6;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            _s = ")))(((";
+            _k = 2;
+
+            var actual = _solution.Solve(_s, _k);
+            var expected = 4;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            _s = ")))(((";
+            _k = 0;
+
+            var actual = _solution.Solve(_s, _k);
+            var expected = 0;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
