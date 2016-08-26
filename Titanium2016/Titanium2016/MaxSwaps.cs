851813e84a8fd2e3b7e0fdde164ac10e14d19641
cheng93 <derek.c@hotmail.co.uk>
2016-08-26 23:24:33 +0100

    max swaps

---
+++ Titanium2016/Titanium2016/MaxSwaps.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Titanium2016
+{
+    public class MaxSwaps
+    {
+        public static int Calculate(IList<int> openBrackets, IList<int> closedBrackets)
+        {
+            var open = openBrackets.Count;
+            var closed = closedBrackets.Count;
+
+            if (open == 0)
+            {
+                if (closed == 0)
+                {
+                    return 0;
+                }
+                return closed / 2;
+            }
+
+            if (closed == 0)
+            {
+                return open / 2;
+            }
+
+            if ((open + closed) % 2 == 1 || open % 2 == 0)
+            {
+                return (open / 2) + (closed / 2);
+            }
+
+            return 2 + (open / 2) + (closed / 2);
+        }
+    }
+}
