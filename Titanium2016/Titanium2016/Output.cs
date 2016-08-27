59a264900ac0769895c790d01d89fcd02a04c998
cheng93 <derek.c@hotmail.co.uk>
2016-08-27 18:02:19 +0100

    Fix bugs

--- Titanium2016/Titanium2016/Output.cs
+++ Titanium2016/Titanium2016/Output.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Titanium2016
 {
@@ -6,7 +8,31 @@ namespace Titanium2016
     {
         public static int Calculate(int length, IList<int> openBrackets, IList<int> closedBrackets)
         {
-            return 0;
+            if (!openBrackets.Any() && !closedBrackets.Any())
+            {
+                return length;
+            }
+
+            var joined = closedBrackets.ToList();
+            joined.AddRange(openBrackets);
+
+            var output = joined[0];
+
+            for (var i = 1; i < joined.Count; i++)
+            {
+                var current = joined[i];
+                var prev = joined[i - 1];
+
+                var dif = current - prev - 1;
+
+                output = Math.Max(output, dif);
+            }
+
+            var end = length - joined.Last() - 1;
+
+            output = Math.Max(output, end);
+
+            return output;
         }
     }
 }
