5c17fe1a73c0ca3b340256a506fb077a0b5fdb08
cheng93 <derek.c@hotmail.co.uk>
2016-08-27 21:04:55 +0100

    Tests

--- Titanium2016/Titanium2016.Test/SolutionsTest.cs
+++ Titanium2016/Titanium2016.Test/SolutionsTest.cs
@@ -257,5 +257,219 @@ namespace Titanium2016.Test
 
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void OpenOnly_Even()
+        {
+            _s = "((((((((";
+            _k = 0;
+
+            var expected = 0;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 1;
+
+            expected = 2;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 2;
+
+            expected = 4;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 3;
+
+            expected = 6;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 4;
+
+            expected = 8;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void OpenOnly_Odd()
+        {
+            _s = "(((((((((";
+            _k = 0;
+
+            var expected = 0;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 1;
+
+            expected = 2;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 2;
+
+            expected = 4;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 3;
+
+            expected = 6;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 4;
+
+            expected = 8;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 5;
+
+            expected = 8;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ClosedOnly_Even()
+        {
+            _s = "))))))))";
+            _k = 0;
+
+            var expected = 0;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 1;
+
+            expected = 2;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 2;
+
+            expected = 4;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 3;
+
+            expected = 6;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 4;
+
+            expected = 8;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ClosedOnly_Odd()
+        {
+            _s = ")))))))))";
+            _k = 0;
+
+            var expected = 0;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 1;
+
+            expected = 2;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 2;
+
+            expected = 4;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 3;
+
+            expected = 6;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 4;
+
+            expected = 8;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 5;
+
+            expected = 8;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AdjacentSlices()
+        {
+            _s = "(()())())))(((";
+            _k = 0;
+
+            var expected = 8;
+            var actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 1;
+
+            expected = 10;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 2;
+
+            expected = 10;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 3;
+
+            expected = 12;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+
+            _k = 4;
+
+            expected = 14;
+            actual = _solution.Solve(_s, _k);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
