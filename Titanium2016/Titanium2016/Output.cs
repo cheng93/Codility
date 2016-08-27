a0832717fc1765e586066bdbad7870c820b6e1d1
cheng93 <derek.c@hotmail.co.uk>
2016-08-27 17:47:47 +0100

    Output

---
+++ Titanium2016/Titanium2016/Output.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Titanium2016
+{
+    public class Output
+    {
+        public static int Calculate(int length, IList<int> openBrackets, IList<int> closedBrackets)
+        {
+            return 0;
+        }
+    }
+}
