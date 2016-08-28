14c32f51081210412760654f2b66de4816602dbb
cheng93 <derek.c@hotmail.co.uk>
2016-08-28 12:36:24 +0100

    use maxSwaps again

--- Titanium2016/Titanium2016/Solution.cs
+++ Titanium2016/Titanium2016/Solution.cs
@@ -1,4 +1,6 @@
-﻿namespace Titanium2016
+﻿using System;
+
+namespace Titanium2016
 {
     public class Solution
     {
@@ -12,7 +14,12 @@
             // O(N)
             var bracketCollections = BracketCollections.Create(s);
 
-            return AnotherSolver.Solve(s, k, bracketCollections);
+            var maxSwaps = MaxSwaps.Calculate(s.Length, bracketCollections.OpenBrackets,
+                bracketCollections.ClosedBrackets);
+
+            var swaps = Math.Min(k, maxSwaps);
+
+            return AnotherSolver.Solve(s, maxSwaps, bracketCollections);
         }
     }
 }
