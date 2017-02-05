b1b702300f07cc5f4ac6e07f203d5e9029189528
cheng93 <derek.c@hotmail.co.uk>
2017-02-05 23:36:55 +0000

    initial solution and tests

---
+++ Chromium2017/Chromium2017.UnitTest/CodilityTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Chromium2017.UnitTest
+{
+    [TestFixture]
+    public class CodilityTests
+    {
+        private Solution SolutionFactory()
+        {
+            return new Solution();
+        }
+
+        [Test]
+        public void Example1()
+        {
+            var solution = SolutionFactory();
+
+            var input = new[] {13, 2, 5};
+
+            var actual = solution.Solve(input);
+            var expected = 7;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var solution = SolutionFactory();
+
+            var input = new[] {4, 6, 2, 1, 5};
+
+            var actual = solution.Solve(input);
+            var expected = 23;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Chromium2017/Chromium2017/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace Chromium2017
+{
+    public class Solution
+    {
+        public int Solve(int[] H)
+        {
+            return 0;
+        }
+    }
+}
