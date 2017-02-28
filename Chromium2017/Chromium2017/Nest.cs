1a9e191256e3895ba46304158b3844c043fd18be
cheng93 <derek.c@hotmail.co.uk>
2017-02-28 11:24:16 +0000

    n^3 solution

---
+++ Chromium2017/Chromium2017/Nest.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chromium2017
+{
+    internal class Nest
+    {
+        public IEnumerable<int> Left
+        {
+            get { return _nests.Take(_homeIndex); }
+        }
+
+        public IEnumerable<int> Right
+        {
+            get { return _nests.Skip(_homeIndex + 1); }
+        }
+
+        public IEnumerable<int> LeftIndexes
+        {
+            get { return NestIndexes.Take(_nests.Take(_homeIndex).Count(x => x > _nests[_homeIndex])); }
+        }
+
+        public IEnumerable<int> RightIndexes
+        {
+            get { return NestIndexes.Skip(LeftIndexes.Count()); }
+        }
+
+        public ILookup<int, int> LeftIndexLookup
+        {
+            get { return ToLookup(LeftIndexes); }
+        }
+
+        public ILookup<int, int> RightIndexLookup
+        {
+            get { return ToLookup(RightIndexes); }
+        }
+
+        public bool SmallestIsLeft
+        {
+            get { return !RightIndexes.Any() || (LeftIndexes.Any() && LeftIndexes.Min() < RightIndexes.Min()); }
+        }
+
+        private readonly int[] _nests;
+
+        private readonly int _homeIndex;
+
+        private IEnumerable<int> NestIndexes
+        {
+            get
+            {
+                var orderedIndexes = _nests
+                    .Where(x => x > _nests[_homeIndex])
+                    .OrderBy(x => x)
+                    .Select((x, i) => new
+                    {
+                        Index = i,
+                        Value = x
+                    })
+                    .ToDictionary(x => x.Value, x => x.Index);
+
+                return _nests
+                        .Where(x => x > _nests[_homeIndex])
+                        .Select(x => orderedIndexes[x]);
+            }
+        }
+
+        public Nest(int[] nests, int homeIndex)
+        {
+            _nests = nests;
+            _homeIndex = homeIndex;
+        }
+
+        private ILookup<int, int> ToLookup(IEnumerable<int> ints)
+        {
+            return ints
+                .OrderBy(x => x)
+                .Select((x, i) => new
+                {
+                    Value = x,
+                    Key = x - i
+                })
+                .ToLookup(x => x.Key, x => x.Value);
+        }
+    }
+}
--- Chromium2017/Chromium2017/Solution.cs
+++ Chromium2017/Chromium2017/Solution.cs
@@ -1,4 +1,7 @@
-﻿namespace Chromium2017
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chromium2017
 {
     public class Solution
     {
@@ -9,7 +12,70 @@
                 return 1;
             }
 
-            return 0;
+            var output = input.Length;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var nest = new Nest(input, i);
+                output = (output + Solve(nest)) % 1000000007;
+            }
+
+            return output;
+        }
+
+        private int Solve(Nest nest)
+        {
+            var left = new List<int>();
+            var right = new List<int>();
+
+            var leftKeys = GetKeysDictionary(nest.LeftIndexLookup);
+            var rightKeys = GetKeysDictionary(nest.RightIndexLookup);
+
+            if (nest.SmallestIsLeft)
+            {
+                for (var i = 0; i < leftKeys.Count; i++)
+                {
+                    var leftSize = nest.LeftIndexLookup[leftKeys[i]].LongCount();
+                    var leftCompute = leftSize * (1 + right.Aggregate(0, (x, y) => (x + y) % 1000000007));
+                    left.Add((int) (leftCompute % 1000000007));
+
+                    if (i < rightKeys.Count)
+                    {
+                        var rightSize = nest.RightIndexLookup[rightKeys[i]].LongCount();
+                        var rightCompute = rightSize * (1 + left.Aggregate(0, (x, y) => (x + y) % 1000000007));
+                        right.Add((int) (rightCompute % 1000000007));
+                    }
+                }
+            }
+            else
+            {
+                for (var i = 0; i < rightKeys.Count; i++)
+                {
+                    var rightSize = nest.RightIndexLookup[rightKeys[i]].LongCount();
+                    var rightCompute = rightSize * (1 + left.Aggregate(0, (x, y) => (x + y) % 1000000007));
+                    right.Add((int)(rightCompute % 1000000007));
+
+                    if (i < leftKeys.Count)
+                    {
+                        var leftSize = nest.LeftIndexLookup[leftKeys[i]].LongCount();
+                        var leftCompute = leftSize * (1 + right.Aggregate(0, (x, y) => (x + y) % 1000000007));
+                        left.Add((int)(leftCompute % 1000000007));
+                    }
+                }
+            }
+
+            return (left.Aggregate(0, (x, y) => (x + y) % 1000000007) + right.Aggregate(0, (x, y) => (x + y) % 1000000007)) % 1000000007;
+        }
+
+        private IDictionary<int, int> GetKeysDictionary(ILookup<int, int> lookup)
+        {
+            return lookup
+                .Select((x, i) => new
+                {
+                    Index = i,
+                    Key = x.Key
+                })
+                .ToDictionary(x => x.Index, x => x.Key);
         }
     }
 }
